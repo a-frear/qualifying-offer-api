301b2981b8a7b4591c7effbd7dbe347862939ac5
Amy <amy.frear@gmail.com>
2021-04-16 12:32:47 -0400

    initial commit

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+const { NODE_ENV, CLIENT_ORIGIN } = require("./config");
+const request = require('request')
 
 const app = express()
 
@@ -13,20 +14,23 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
-
-app.get('/', (req,res) => {
-    res.send('Hello, world!')
-})
-
-app.use((error, req, res, next) => {
-    let response
-    if (NODE_ENV === 'production') {
-      response = { error: { message: 'server error' }}
-    } else {
-      response = { error }
-    }
-    res.status(500).json(response)
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN,
   })
+);
+
+app.get('/', (req, res) => {
+  request(
+    { url: 'https://questionnaire-148920.appspot.com/swe/data.html' },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500);
+      }
+
+      res.send(body)
+    }
+  )
+});
 
 module.exports = app
