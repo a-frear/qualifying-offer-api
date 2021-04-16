2fdddbde7da16b480a5c780324522363c6b35d78
Amy <amy.frear@gmail.com>
2021-04-16 12:38:28 -0400

    updated client origin

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    CLIENT_ORIGIN: 'https://qo-generator.vercel.app'
+    CLIENT_ORIGIN: 'https://qo-generator-a-frear.vercel.app/'
   }
